94ef764a8e6428bb9a4b2e4ba87f4dc96b367f2c
xXDuckXx <dacs2010@gmail.com>
2018-11-25 22:51:59 -0500

    categorys page is kinda working, kinda

--- books/views.py
+++ books/views.py
@@ -2,24 +2,27 @@ from django.shortcuts import render
 from books.models import Book, Category
 
 
+
 # Create your views here.
-def category_detail(request, slug):
-    '''
-    category pages
-    '''
-    categorys = Category.objects.all()
-    return render(request, 'categorys/category_detail.html', {
-        'categorys': categorys,
-    })
-
-
 def index(request):
     '''
     render index.html
     '''
     categorys = Category.objects.all()
-    books = Book.objects.all()
+    books = Book.objects.all().order_by('-date')
     return render(request, 'books/index.html', {
+        'books': books,
+        'categorys': categorys,
+    })
+
+
+def category_detail(request, slug):
+    '''
+    category pages
+    '''
+    categorys = Category.objects.all()
+    books = Book.objects.all()
+    return render(request, 'categorys/category_detail.html', {
         'books': books,
         'categorys': categorys,
     })
